4e90079fe560280383596cdbb75d7d489317b90e
KunalBodakhe <kunalbodakhe1230@gmail.com>
2025-03-15 15:18:05 +0530

    Expense Ttracker Started

---
+++ Expense Tracker/src/components/header-name.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Headername(){
+    
+    
+    return(
+        
+            
+                <h1 className="main-header">Expense Tracker</h1>
+            
+        
+    )
+}
+export default Headername;
