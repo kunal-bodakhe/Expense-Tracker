eaaefd2e9ee09c793e9e5ce3fc36cf6607b2348e
KunalBodakhe <kunalbodakhe1230@gmail.com>
2025-04-27 13:53:18 +0530

    Added Expense list function

--- Expense Tracker/src/components/AddExpensePopup.jsx
+++ Expense Tracker/src/components/AddExpensePopup.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import { useState } from 'react';
+
 import closeButton from "../assets/closeButton.png";
 
 function AddExpensePopup({ handleExpensePopup }) {
-  return (
-    <>
 
+  const [description, setDescription] = useState("");
+  const [expenseAmount, setExpenseAmount] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const expense = {
+      description,
+      expenseAmount,
+      category,
+    };
+    const expenses = JSON.parse(localStorage.getItem("expenses")) || [];
+    expenses.push(expense);
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+    handleExpensePopup(false);
+  };
+
+  
+  return (
+    
+    <>
       <div className="overlay">
         <div className="popup">
           <h4>New Expense</h4>
@@ -14,6 +35,8 @@ function AddExpensePopup({ handleExpensePopup }) {
               type="text"
               name="description"
               placeholder="Expense description"
+              value={description}
+              onChange={(e)=>setDescription(e.target.value)}
             />
           </div>
           <div>
@@ -22,18 +45,20 @@ function AddExpensePopup({ handleExpensePopup }) {
               type="number"
               name="ExpenseAmount"
               placeholder="In dollars"
+              value={expenseAmount}
+              onChange={(e)=>setExpenseAmount(e.target.value)}
             />
           </div>
           <div>
             Category:
-            <select>
-              <option>Food</option>
-              <option>Groceries</option>
-              <option>Travel</option>
-              <option>Health</option>
+            <select defaultValue={category} onClick={(e)=>setCategory(e.target.value)}>
+              <option value="Food">Food</option>
+              <option value="Groceries">Groceries</option>
+              <option value="Travel">Travel</option>
+              <option value="Health">Health</option>
             </select>
           </div>
-          <button className="submitButton">Submit</button>
+          <button className="submitButton" onClick={handleSubmit}>Submit</button>
           <button className="closeButton"><img  src={closeButton} onClick={()=>{handleExpensePopup(false)}}></img></button>
         </div>
       </div>
--- Expense Tracker/src/components/expenseList.jsx
+++ Expense Tracker/src/components/expenseList.jsx
@@ -1,51 +1,73 @@
 import React from "react";
 
-function ExpenseList({handleEditPopup , handleDeletePopup}){
+function ExpenseList({ handleEditPopup, handleDeletePopup }) {
 
-    return(
-        <>
-        <div className="container"> 
-            <div className="expenseChart">
-                <div className="expenseContent">
-                    <div>
-                        <h4>SrNo.</h4>
-                    </div>
-                    <div className="expenseTitle">
-                        <h4>Expense</h4>
-                    </div>
-                    <div className="amountTitle">
-                        <h4>Amount</h4>
-                    </div>
-                </div>
-                <div className="editExpense">
-                    <div>
-                        <h4>Edit/Delete</h4>
-                    </div>
-                </div>
+    let expenses=JSON.parse(localStorage.getItem("expenses"));
+  return (
+    <>
+      <div className="container">
+        <div className="expenseChart">
+          <div className="expenseContent">
+            <div>
+              <h4>SrNo.</h4>
             </div>
-            <hr className="seperationLine"></hr>
-            <div  className="expenseList">
-                <div className="expenseDetails">
-                    <div className="srNo">
-                        <h4>1.</h4>
-                    </div>
-                    <div className="expense">
-                        <h4>Expense of shopping</h4>
-                    </div>
-                    <div className="expenseAmount">
-                        <h4>$200</h4>
-                    </div>
-                </div>
-                <div>
-                    <div className="editOptions">
-                        <button className="editButtons" onClick={()=>{handleEditPopup(true)}}>Edit</button>
-                        <button className="editButtons" onClick={()=>{handleDeletePopup(true)}}>Delete</button>
-                    </div>
-                </div>
+            <div className="expenseTitle">
+              <h4>Expense</h4>
             </div>
+            <div className="amountTitle">
+              <h4>Amount</h4>
+            </div>
+          </div>
+          <div className="editExpense">
+            <div>
+              <h4>Edit/Delete</h4>
+            </div>
+          </div>
         </div>
-        </>
-    )
+        <hr className="seperationLine"></hr>
+
+        
+          <div>
+            {expenses.map((expense, index) => (
+              <div className="expenseList" key={index}>
+                <div className="expenseDetails">
+                  <div className="srNo">
+                    <h4>{index + 1}.</h4>
+                  </div>
+                  <div className="expense">
+                    <h4>{expense.description}</h4>
+                  </div>
+                  <div className="expenseAmount">
+                    <h4>${expense.expenseAmount}</h4>
+                  </div>
+                </div>
+
+                <div>
+                  <div className="editOptions">
+                    <button
+                      className="editButtons"
+                      onClick={() => {
+                        handleEditPopup(true, expense);
+                      }}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className="editButtons"
+                      onClick={() => {
+                        handleDeletePopup(true, expense);
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+    </>
+  );
 }
 
 export default ExpenseList;
